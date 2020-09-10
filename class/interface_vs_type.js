9b8e3691e9f08499ef931aac37fce4cf376666d8
Tamm <tmmoond8@gmail.com>
2020-09-10 14:40:54 +0900

    study type overload

--- class/interface_vs_type.js
+++ class/interface_vs_type.js
@@ -3,3 +3,37 @@ const mask = {
     color: "White",
     size: 12,
 };
+const createElement = (tag) => {
+    if (tag === "a")
+        return new HTMLAnchorElement();
+    if (tag === "table")
+        return new HTMLTableCaptionElement();
+    return new HTMLElement();
+};
+const numberChange = (input) => {
+    if (typeof input === "string")
+        return {};
+    else
+        return input.toString();
+};
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+function pickCard(x) {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+const callback = function callback(message, line) {
+    console.log(message + "call");
+    callback.isfulfilled = true;
+};
+callback.isfulfilled = false;
+callback("foo");
+console.dir(callback);
--- class/interface_vs_type.ts
+++ class/interface_vs_type.ts
@@ -69,3 +69,59 @@ const mask: Mask = {
 interface Notebook<Age extends number> {
   age: Age;
 }
+
+type CreateElement = {
+  (tag: "a"): HTMLAnchorElement;
+  (tag: "canvas"): HTMLCanvasElement;
+  (tag: "table"): HTMLTableElement;
+  (tag: string): HTMLElement;
+};
+
+const createElement: CreateElement = (tag: any): any => {
+  if (tag === "a") return new HTMLAnchorElement();
+  if (tag === "table") return new HTMLTableCaptionElement();
+  return new HTMLElement();
+};
+
+type NumberChange = {
+  (input: string): number;
+  (input: number): string;
+};
+
+const numberChange: NumberChange = (input: any) => {
+  if (typeof input === "string") return {};
+  else return input.toString();
+};
+
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x: { suit: string; card: number }[]): number;
+function pickCard(x: number): { suit: string; card: number };
+function pickCard(x: any): any {
+  // Check to see if we're working with an object/array
+  // if so, they gave us the deck and we'll pick the card
+  if (typeof x == "object") {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  }
+  // Otherwise just let them pick the card
+  else if (typeof x == "number") {
+    let pickedSuit = Math.floor(x / 13);
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
+}
+
+type Callback = {
+  (message: string): void;
+  (message: string, line: number): void;
+  isfulfilled: boolean;
+};
+
+const callback: Callback = function callback(message: string, line?: number) {
+  console.log(message + "call");
+  callback.isfulfilled = true;
+};
+callback.isfulfilled = false;
+
+callback("foo");
+console.dir(callback);
